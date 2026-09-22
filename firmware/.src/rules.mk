fddf54b4811fc2203f7b8d67ca209feee0be6259
Mikko Turunen <57153670+makkomise@users.noreply.github.com>
2026-09-22 12:41:30 +0300

    Firmware
    - Keymap
    - Custom animation for oled
    - Cleaned configs and rules a bit

---
+++ firmware/.src/rules.mk
@@ -0,0 +1,18 @@
+MCU = RP2040
+BOOTLOADER = rp2040
+
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+NKRO_ENABLE = yes            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+AUDIO_ENABLE = no           # Audio output
+
+ENCODER_ENABLE = yes       # ENables the use of one or more encoders
+LTO_ENABLE = yes 		   # significantly reduce the compiled size, but disable the legacy TMK Macros and Functions features
+
+OLED_DRIVER = ssd1306    # OLED display
+SERIAL_DRIVER = vendor
+RGB_MATRIX_ENABLE = yes
